b0ee1afea7137054c8063c5a9e60a8b1291bdf1d
fmavarani <maa.ravin@gmail.com>
2023-04-12 09:59:45 +0100

    Added options for pictures and office documents

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ def sort_files_by_extension_and_date(dir_path):
             destination_path = os.path.join(year_dir, file)
             shutil.move(source_path, destination_path)
 
-def sort_documents_by_extension_and_date(dir_path):
+def sort_pics_by_extension_and_date(dir_path):
     # create directories for each file extension
     sortedPath=dir_path
     for root, dirs, files in os.walk(dir_path):
         for file in files:
-            if file.lower().endswith((".doc", ".docx", ".docm", ".dot", ".dotx", ".dotm", ".xls", ".xlsx", ".xlsm", ".xlsb", ".xlt", ".xltx", ".xltm", ".ppt", ".pptx", ".pptm", ".pot", ".potx", ".potm", ".ppa", ".ppam", ".pps", ".ppsx", ".ppsm", ".sldx", ".sldm", ".accdb", ".accdt", ".accdr", ".pub", 'pdf', 'blend', 'apk')):
+            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
                 # get file extension
-                file_extension = os.path.splitext(file)[1][1:].lower()
+                file_extension="pictures"
             
                 # get year the file was created
                 file_path = os.path.join(root, file)
@@ -66,7 +66,39 @@ def sort_documents_by_extension_and_date(dir_path):
                 # move file to year directory
                 destination_path = os.path.join(year_dir, file)
                 shutil.move(source_path, destination_path)
+                
+def sort_documents_by_extension_and_date(dir_path):
+    # create directories for each file extension
+    sortedPath=dir_path
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if file.lower().endswith((".doc", ".docx", ".docm", ".dot", ".dotx", ".dotm", ".xls", ".xlsx", ".xlsm", ".xlsb", ".xlt", ".xltx", ".xltm", ".ppt", ".pptx", ".pptm", ".pot", ".potx", ".potm", ".ppa", ".ppam", ".pps", ".ppsx", ".ppsm", ".sldx", ".sldm", ".accdb", ".accdt", ".accdr", ".pub", 'pdf')):
+                # get file extension
+                file_extension="Office Documents"
+            
+                # get year the file was created
+                file_path = os.path.join(root, file)
+                creation_time = os.path.getmtime(file_path)
+                year = datetime.datetime.fromtimestamp(creation_time).strftime('%Y')
 
+                
+                # create directory for file extension if it doesn't exist
+                extension_dir = os.path.join(sortedPath, file_extension)
+                if not os.path.exists(extension_dir):
+                    os.makedirs(extension_dir)
+
+                # create directory for year if it doesn't exist
+                year_dir = os.path.join(extension_dir, year)
+                if not os.path.exists(year_dir):
+                    os.makedirs(year_dir)
+
+                # move file to extension directory
+                source_path = os.path.join(root, file)
+                print(source_path)
+                # move file to year directory
+                destination_path = os.path.join(year_dir, file)
+                shutil.move(source_path, destination_path)
+                
 def delete_empty_folders(path):
     for root, dirs, files in os.walk(path, topdown=False):
         for name in dirs:
@@ -94,6 +126,9 @@ if __name__ == '__main__':
             delete_empty_folders(dir_path)
         elif command.lower() == "all":
             sort_files_by_extension_and_date(dir_path)
+            delete_empty_folders(dir_path)
+        elif command.lower() == "pics":
+            sort_pics_by_extension_and_date(dir_path)
             delete_empty_folders(dir_path)
     else:    
         print("Please input proper command argument")
